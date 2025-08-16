37332a8726bab5a76250fe3838597e7785f38b05
null <null>
2025-08-16 09:39:01 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
-export default App
+export default function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchPosts = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data: Post[] = await res.json();
+      setPosts(data);
+    } catch (error) {
+      console.error("Ошибка при загрузке постов:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+    const interval = setInterval(fetchPosts, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="title">Posts:</h1>
+      {posts.map((post) => (
+        <div key={post.id} className="card">
+          <h2 className="post-title">{post.title}</h2>
+          <p className="post-body">{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
